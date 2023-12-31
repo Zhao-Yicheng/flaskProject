c42b848ae9665cbe3ac2166a2d3f18b491f9b116
Zhao Yicheng <15990732621@163.com>
2023-12-31 18:11:52 +0800

    1

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
-from flask import Flask
+from flask import Flask,render_template,request,redirect,url_for
 import flask
+from sql import *
 app = Flask(__name__)
 
 
+@app.route("/")
+def hello_world():
+    return redirect('login')
 
-@app.route('/login')
-def hello_world():  # put application's code here
-    return flask.render_template('login.html')
+@app.route('/login',methods=['GET','POST'])
+def user_login():
+    if request.method=='POST':  # 注册发送的请求为POST请求
+        username = request.form['username']
+        password = request.form['password']
+        if username=='' and password=='':
+            login_massage = "温馨提示：账号和密码是必填"
+            return render_template('login.html', message=login_massage)
+        elif is_existed(username, password):
+            return render_template('index.html', username=username)
+        elif exist_user(username):
+            login_massage = "温馨提示：密码错误，请输入正确密码"
+            return render_template('login.html', message=login_massage)
+        else:
+            login_massage = "温馨提示：不存在该用户，请先注册"
+            return render_template('login.html', message=login_massage)
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ sql.py
@@ -0,0 +1,8 @@
+def is_existed(username,password):
+    if username == 'test' and password == '123':
+        return True
+    return False
+def exist_user(username):
+    if username == 'test' :
+        return  True
+    return False
